fa4cb00b2bab2802c2a7fadea17206b19c9517c0
twoandtwelve <jackykim573@gmail.com>
2024-03-10 21:41:53 +1300

    Add project files.

---
+++ NoLateNotification/Patches/NotificationPatch.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NoLateNotification.Patches
+{
+    [HarmonyPatch(typeof(TimeOfDay))]
+
+    internal class NotificationPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void notificationPatch(ref bool ___shipLeavingAlertCalled)
+        {
+            if (NoLateNotification.Instance.isNoLateNotificationEnabledEntry.Value)
+            {
+                ___shipLeavingAlertCalled = true;
+            }
+        }
+    }
+}
---
+++ NoLateNotification/Plugin.cs
@@ -0,0 +1,53 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using LethalConfig;
+using LethalConfig.ConfigItems;
+using NoLateNotification.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoLateNotification
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    [BepInDependency("ainavt.lc.lethalconfig")]
+    public class NoLateNotification : BaseUnityPlugin
+    {
+        private const string modGUID = "Jacky.NoLateNotification";
+        private const string modName = "NolateNotification";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        public static NoLateNotification Instance;
+
+        public static ManualLogSource logger;
+
+        public ConfigEntry<bool> isNoLateNotificationEnabledEntry;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            logger = base.Logger;
+
+            logger.LogInfo("NoLateNotification mod has awaken");
+
+            isNoLateNotificationEnabledEntry = Config.Bind("NoLateNotification Config", "Disable Time Late Notification", true, "This disables the ship is leaving soon notification!");
+    
+            BaseConfigItem isNoLateNotificationEnabled = new BoolCheckBoxConfigItem(isNoLateNotificationEnabledEntry, requiresRestart: false);
+
+            LethalConfigManager.AddConfigItem(isNoLateNotificationEnabled);
+
+
+            harmony.PatchAll(typeof(NotificationPatch));
+        }
+    }
+}
